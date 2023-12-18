0fc5e03cc2539b6691ab5ee86a0dd68b73da6a5f
Luca Gheorghe <lucatgheorghe9@gmail.com>
2023-12-18 21:02:04 +0200

    reconfigurare parametrii perlin noise - script.js

--- 231218-perlin_noise_circular/script.js
+++ 231218-perlin_noise_circular/script.js
@@ -16,21 +16,21 @@ function draw() {
     center()
     //centreaza canvas
 
-    var space = 0.1;
+    var space = 1.25;
     for (var i = 0; i < 360; i += space){
-        var x_offset = map(sin(i), 10, -1, 10, 20)
+        var x_offset = map(sin(i), 10, -1, 10, 50)
         var y_offset = map(cos(i), 50, -1, 50, 100)
         //perlin noise
         var p = noise(x_offset + start, y_offset + start)
         var h = map(p, 0, 1, -150, 150)
 
-        var r = map(sin(i), -1, 1, 30, 200)
-        var g = map(h, -50, 50, 0, 0)
-        var b = map(p, 0, 1, 50, 50)
+        var r = map(sin(i), -1, 1, 30, 10)
+        var g = map(h, -50, 250, 0, 10)
+        var b = map(p, 0, 1, 50, 175)
         
         rotate(space);
         fill(r, g, b);
-        rect(200, 0, h, 1);
+        rect(200, 0, h, 2);
     }
 
     start += 0.01
